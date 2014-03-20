2e6d3eff2c7862fd440f71dee822b8e2fefb6f2a
JD Trout <jdtrout@ucla.edu>
2014-03-20 13:31:57 -0700

    Added specific endpoints for meraki

--- routes/radcheck.rb
+++ routes/radcheck.rb
@@ -2,37 +2,47 @@ class Api < Sinatra::Application
 
 	get "/radcheck/?" do
 		content_type :json
-		
+
 		radcheck = RadCheck
 		radcheck = build_filters(radcheck, params)
 		radcheck = build_conditions(radcheck, params)
 		extract_radusers(radcheck).to_json
 	end
 
+	# The client needs to send username, attribute (password encryption) & password as value (if encrypted pre-encrypt)
+	# JSON example: {"attribute" : "SHA-Password", "username" : "boblong", "value" : "myencpytedpassword"}
 	post "/radcheck" do
 		content_type :json
 
 		ensure_payload do |session_hash|
+			session_hash["op"] = ":="
 			new_user = RadCheck.create(session_hash)
 
 			body json(new_user.to_hash)
 		end
 	end
 
-	put "/radcheck/:id" do
+	put "/radcheck/:username" do
 		content_type :json
 		
 		ensure_payload do |session_hash|
-			updated_user = RadCheck[params[:id]].update(session_hash)
+			updated_user = RadCheck.where(username: params[:username]).update(session_hash)
 
-			body json(updated_user.to_hash)
+			if updated_user == 1
+				status 200
+				body json(status: "Success")
+			elsif updated_user == 0
+				status 400
+				body json(status: "Bad syntax or record not found")
+			end
 		end
 	end
 
-	delete "/radcheck/:id" do
+	delete "/radcheck/:username" do
 		content_type :json
-		
-		deleted_user = RadCheck[params[:id]].delete
-		body json(deleted_user.to_hash)
+
+		deleted_user = RadCheck.where(username: params[:username]).delete
+		status 204
+		body ''
 	end
 end
--- routes/radreply.rb
+++ routes/radreply.rb
@@ -1,8 +1,9 @@
 class Api < Sinatra::Application
 
+	#open
 	get "/radreply/?" do
 		content_type :json
-		
+
 		radreply = RadReply
 		radreply = build_filters(radreply, params)
 		radreply = build_conditions(radreply, params)
@@ -21,7 +22,7 @@ class Api < Sinatra::Application
 
 	put "/radreply/:id" do
 		content_type :json
-		
+
 		ensure_payload do |session_hash|
 			updated_user = RadReply[params[:id]].update(session_hash)
 
@@ -31,8 +32,110 @@ class Api < Sinatra::Application
 
 	delete "/radreply/:id" do
 		content_type :json
-		
+
 		deleted_user = RadReply[params[:id]].delete
 		body json(deleted_user.to_hash)
+	end
+
+	#session_time
+	post "/radreply/:username/session_timeout" do
+		content_type :json
+
+		ensure_payload do |session_hash|
+			new_user = RadReply.create(username: params[:username], attribute: "Session-Timeout", op: ":=", value: session_hash["value"])
+
+			body json(new_user.to_hash)
+		end
+	end
+
+	put "/radreply/:username/session_timeout" do
+		content_type :json
+
+		ensure_payload do |session_hash|
+			updated_user = RadReply.where(username: params[:username], attribute: "Session-Timeout").update(value: session_hash["value"])
+			if updated_user == 1
+				status 200
+				body json(status: "Success")
+			elsif updated_user == 0
+				status 400
+				body json(status: "Bad syntax or record not found")
+			end
+		end
+	end
+
+	delete "/radreply/:username/session_timeout" do
+		content_type :json
+
+		deleted_user = RadReply.where(username: params[:username], attribute: "Session-Timeout").delete
+		status 204
+		body ''
+	end
+
+	#maximum_data_rate_upstream
+	post "/radreply/:username/maximum_data_rate_upstream" do
+		content_type :json
+
+		ensure_payload do |session_hash|
+			new_user = RadReply.create(username: params[:username], attribute: "Maximum-Data-Rate-Upstream", op: ":=", value: session_hash["value"])
+
+			body json(new_user.to_hash)
+		end
+	end
+
+	put "/radreply/:username/maximum_data_rate_upstream" do
+		content_type :json
+
+		ensure_payload do |session_hash|
+			updated_user = RadReply.where(username: params[:username], attribute: "Maximum-Data-Rate-Upstream").update(value: session_hash["value"])
+			if updated_user == 1
+				status 200
+				body json(status: "Success")
+			elsif updated_user == 0
+				status 400
+				body json(status: "Bad syntax or record not found")
+			end
+		end
+	end
+
+	delete "/radreply/:username/maximum_data_rate_upstream" do
+		content_type :json
+
+		deleted_user = RadReply.where(username: params[:username], attribute: "Maximum-Data-Rate-Upstream").delete
+		status 204
+		body ''
+	end
+
+	#maximum_data_rate_downstream
+	post "/radreply/:username/maximum_data_rate_downstream" do
+		content_type :json
+
+		ensure_payload do |session_hash|
+			new_user = RadReply.create(username: params[:username], attribute: "Maximum-Data-Rate-Downstream", op: ":=", value: session_hash["value"])
+
+			body json(new_user.to_hash)
+		end
+	end
+
+	put "/radreply/:username/maximum_data_rate_downstream" do
+		content_type :json
+
+		ensure_payload do |session_hash|
+			updated_user = RadReply.where(username: params[:username], attribute: "Maximum-Data-Rate-Downstream").update(value: session_hash["value"])
+			if updated_user == 1
+				status 200
+				body json(status: "Success")
+			elsif updated_user == 0
+				status 400
+				body json(status: "Bad syntax or record not found")
+			end
+		end
+	end
+
+	delete "/radreply/:username/maximum_data_rate_downstream" do
+		content_type :json
+
+		deleted_user = RadReply.where(username: params[:username], attribute: "Maximum-Data-Rate-Downstream").delete
+		status 204
+		body ''
 	end
 end
